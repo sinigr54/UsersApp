ab6a4f40ed18977c0d496359f1505bd9944cd664
Александр Чех <heapify@yandex.ru>
2019-05-17 10:46:55 +0300

    + Some refactoring

--- app/src/main/java/com/sinigr/usersapp/modules/main/users_list/interactor/IUsersListInteractor.kt
+++ app/src/main/java/com/sinigr/usersapp/modules/main/users_list/interactor/IUsersListInteractor.kt
@@ -3,8 +3,6 @@ package com.sinigr.usersapp.modules.main.users_list.interactor
 import com.sinigr.usersapp.base.interactor.ICoroutineInteractor
 import com.sinigr.usersapp.base.interactor.subscriber.ISubscriber
 import com.sinigr.usersapp.entity.UserEntity
-import com.sinigr.usersapp.network.network_manager.OnError
-import com.sinigr.usersapp.network.network_manager.OnSuccessWithData
 
 interface IUsersListInteractor : ICoroutineInteractor {
     fun loadUsers(subscriber: ISubscriber<List<UserEntity>>)
